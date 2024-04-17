605285485f690ee56260530db206e102c355a9ef
zoejungi <zoe.jungi@hispeed.ch>
2024-04-17 09:07:24 +0200

    Add project to github

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample main.py how to analyse and plot data from D-Flow and Vicon
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
